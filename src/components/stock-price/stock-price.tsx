41b38f772bec2cffdc5ca8034fef56f75fd24a9e
Josh Mabry <mabry1985@gmail.com>
2021-02-10 09:33:27 -0800

    add validation check for empty input

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State } from '@stencil/core';
+import { Component, Element, h, State } from '@stencil/core';
 
 import { AV_API_KEY } from '../../global/global';
 
@@ -8,27 +8,45 @@ import { AV_API_KEY } from '../../global/global';
   shadow: true,
 })
 export class StockPrice {
+  stockInput: HTMLInputElement;
+
+  // @Element() el: HTMLElement;
+
   @State() fetchedPrice: number;
+  @State() stockUserInput: string;
+  @State() stockInputValid = false;
+
+  onUserInput = (event: Event) => {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+    if (this.stockUserInput.trim() !== '') {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
+    }
+  }
 
   onFetchStockPrice = (event: Event) => {
     event.preventDefault();
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=${AV_API_KEY}`)
+    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const stockSymbol = this.stockInput.value;
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
       .then(res => {
         return res.json();
       })
       .then(parsedRes => {
-        this.fetchedPrice = +parsedRes['Global Quote']['05. price']
+        this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
   render() {
     return [
       <form onSubmit={this.onFetchStockPrice}>
-        <input id="stock-symbol" />
-        <button type="submit">Fetch</button>
+        <input id="stock-symbol" ref={el => (this.stockInput = el)} value={this.stockUserInput} 
+        onInput={this.onUserInput}/>
+        <button type="submit" disabled={!this.stockInputValid}>Fetch</button>
       </form>,
       <div>
         <p>Price: ${this.fetchedPrice}</p>
